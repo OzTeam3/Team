ad2e58fb21a9c958dca1a07ea7e96888a0bd32e8
홍성욱 <hsw9901@gmail.com>
2026-06-16 19:30:14 +0900

    feat: [함정] 회전 함정 / 사라지는 발판 함정 추가
    스핀 스피드 = 회전 함정 회전 속도 조절 / 온타임 = 발판 활성화 시간, 오프타임 = 발판 비활성화 시간(오프타임 후 다시 활성화)

---
+++ Assets/Su_Trap/Scripts/BlinkFloor.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlinkFloor : MonoBehaviour
+{
+    [Header("타이밍 설정")]
+    [SerializeField] private float _delay = 2.0f; // 게임 시작 후 대기 시간
+    [SerializeField] private float _onTime = 3.0f;
+    [SerializeField] private float _offTime = 3.0f;
+
+    private MeshRenderer _mesh;
+    private Collider _collider;
+    
+    private void Awake()
+    {
+        _mesh = GetComponent<MeshRenderer>();
+        _collider = GetComponent<Collider>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(BlinkLoop());
+    }
+
+    private IEnumerator BlinkLoop()
+    {
+        yield return new WaitForSeconds(_delay);
+
+        while (true)
+        {
+            TurnOn();
+            yield return new WaitForSeconds(_onTime);
+
+            TurnOff();
+
+            yield return new WaitForSeconds(_offTime);
+        }
+    }
+
+    private void TurnOn()
+    {
+        if (_mesh != null) _mesh.enabled = true;
+        if (_collider != null) _collider.enabled = true;
+    }
+
+    private void TurnOff()
+    {
+        if (_mesh != null) _mesh.enabled = false;
+        if (_collider != null) _collider.enabled = false;
+    }
+}
---
+++ Assets/Su_Trap/Scripts/SpinTrap.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class SpinTrap : MonoBehaviour
+{
+    [SerializeField] private float _spinSpeed = 60f;
+
+    //[SerializeField] private Rigidbody _trapRigidbody;
+
+    private void Awake()
+    {
+        //if (GetComponent<Rigidbody>() != null)
+
+        //{
+
+        //    _trapRigidbody = GetComponent<Rigidbody>();
+
+        //}
+    }
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * _spinSpeed * Time.deltaTime);
+    }
+
+    //private void FixedUpdate()
+    //{
+    //    Quaternion deltaSpin = Quaternion.Euler(Vector3.up * _spinSpeed *  Time.fixedDeltaTime);
+
+    //    _trapRigidbody.MoveRotation(_trapRigidbody.rotation * deltaSpin);
+    //}
+}
