549c2fec2b1a61b6113d5802573d704c40afad35
이주헌 <Volume6214@gmail.com>
2026-06-19 13:46:43 +0900

    feat: [AddressableUtil] address 경로관리용 클래스 추가

---
+++ Assets/JH_Scripts/AddressableUtil.cs
@@ -0,0 +1,18 @@
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class AddressableUtil
+{
+    public static class AddressPath
+    {
+        public const string Character = "JsonOutput/Character";
+        public const string Item = "JsonOutput/Item";
+        public const string Trap = "JsonOutput/Trap";
+    }
+
+    public static async UniTask<T> LoadAssetAsync<T>(string address)
+    {
+        return await Addressables.LoadAssetAsync<T>(address).ToUniTask();
+    }
+}
