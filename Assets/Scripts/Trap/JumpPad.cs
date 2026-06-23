de806ad7d34a48256cc3358f3f8d383d67454916
홍성욱 <hsw9901@gmail.com>
2026-06-23 16:18:50 +0900

    feat: [함정] 점프패드 추가 / 팬듈럼 함정 에셋 추가 및 프리팹 변경

---
+++ Assets/Scripts/Trap/JumpPad.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+    [SerializeField] private Vector3 _bounceDirection = new Vector3(1f, 1f, 0f);
+    [SerializeField] private float _bounceForce = 15.0f;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PlayerFeet"))
+        {
+            var receiver = other.GetComponentInParent<KnockbackReceiver>();
+
+            if (receiver != null) 
+            {
+                Vector3 finalDirection = transform.TransformDirection(_bounceDirection).normalized;
+
+                receiver.ApplyKnockback(finalDirection, _bounceForce);
+            }
+        }
+    }
+}
