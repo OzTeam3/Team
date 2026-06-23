3e879bbeb2f4b9a710c76e51e1748ff068104c9d
강혜창 <a4076a4076@gmail.com>
2026-06-23 18:51:14 +0900

    UI: [MainHUD] 메인HUD 구현

---
+++ Assets/Scripts/UI/Canvas/MainHUD.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public class MainHUD : UIBase
+{
+    private Rigidbody _playerRigidbody;
+
+    private void Start()
+    {
+
+        if (GameManager.Instance.GetPlayerRigidbody() == null)
+        {
+            GameManager.Instance.PlayerCreated += BindPlayer;
+        }
+        else
+        {
+            BindPlayer(GameManager.Instance.GetPlayerRigidbody());
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.PlayerCreated -= BindPlayer;
+        }
+    }
+    private void BindPlayer(Rigidbody playerRigidbody)
+    {
+        _playerRigidbody = playerRigidbody;
+    }
+
+
+}
