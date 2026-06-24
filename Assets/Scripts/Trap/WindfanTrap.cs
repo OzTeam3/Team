86056c5c138b422fd51ce9123947c8a2414aad1a
홍성욱 <hsw9901@gmail.com>
2026-06-24 17:42:01 +0900

    feat: [함정] 풍차 플레이어 공중 체공 및 밀어내는 기믹 추가

---
+++ Assets/Scripts/Trap/WindfanTrap.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class FanTrap : MonoBehaviour
+{
+    [Header("바람 설정")]
+    [SerializeField] private float _windStrength = 30f;
+
+    private readonly HashSet<Rigidbody> _entitiesInWind = new();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Rigidbody playerRb = other.GetComponent<Rigidbody>();
+            if (playerRb != null)
+            {
+                _entitiesInWind.Add(playerRb);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Rigidbody playerRb = other.GetComponent<Rigidbody>();
+            if (playerRb != null)
+            {
+                _entitiesInWind.Remove(playerRb);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_entitiesInWind.Count == 0) return;
+
+        Vector3 windForce = transform.forward * _windStrength;
+
+        foreach (Rigidbody rb in _entitiesInWind)
+        {
+            if (rb != null)
+            {
+                rb.AddForce(windForce, ForceMode.Force);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Trap/WindmillBlade.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class FanBladeSpinner : MonoBehaviour
+{
+    [SerializeField] private float _spinSpeed = 720f;
+
+    private void Update()
+    {
+        transform.Rotate(0f, 0f, _spinSpeed * Time.deltaTime);
+    }
+}
