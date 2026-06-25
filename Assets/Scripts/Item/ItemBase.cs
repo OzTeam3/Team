b1584cbc29e24fb6c3d4b607af64bec43c7cfb47
김찬규 <rlacksrb1121@gmail.com>
2026-06-25 21:32:49 +0900

    refactor: [Item] Item 기본틀이 되는 추상클래스

--- Assets/Scripts/Item/ItemBase.cs
+++ Assets/Scripts/Item/ItemBase.cs
@@ -3,11 +3,26 @@ using UnityEngine;
 public enum ItemType
 {
     None,
-    AddBuff    
+    AddBuff
 }
+
+
 public abstract class ItemBase
 {
     protected string _itemName;
-    public abstract void OnAcquire(PlayerView aquirer);
-    public abstract void OnUse();
+    public abstract void AcquireItem(PlayerView player);
+    public abstract void UseItem(PlayerView player);
+}
+
+public class AddStatusItem : ItemBase
+{
+    PlayerView _owner;
+    public override void AcquireItem(PlayerView player)
+    {
+
+    }
+    public override void UseItem(PlayerView player)
+    {
+
+    }
 }
