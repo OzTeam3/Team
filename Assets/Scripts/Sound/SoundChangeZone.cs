96e9ccd0e63961462418faa9bda4a67feb4ee530
강혜창 <a4076a4076@gmail.com>
2026-06-28 20:47:55 +0900

    feat: [UIButton] 버튼 효과와 버튼 효과음 추가, 사운드 추가

---
+++ Assets/Scripts/Sound/SoundChangeZone.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SoundChangeZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") == true)
+        {
+            SoundManager.Instance.StopBGM();
+            SoundManager.Instance.PlayBGM("Assets/Sound/BGM2");
+        }
+        return;
+    }
+}
--- Assets/Scripts/UI/Elements/UIButton.cs
+++ Assets/Scripts/UI/Elements/UIButton.cs
@@ -1,28 +1,45 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class UIButton : MonoBehaviour
+public class UIButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private Button _buttonBase;
     [SerializeField] private Text _textBase;
     [SerializeField] private Image _imageBase;
     [SerializeField] private Image _imageSelect;
+    [SerializeField] private float _hoverScale = 1.1f;
+
+    private Vector3 _originalScale;
 
     private void Awake()
     {
         InitUIButton();
         SetDefaultUI();
+        _originalScale = transform.localScale;
     }
 
     private void OnEnable()
     {
         BindOnClickButtonEvent(OnClickSetSelectUI);
+        BindOnClickButtonEvent(PlayClickSound);
     }
 
     private void OnDisable()
     {
         _buttonBase.onClick.RemoveAllListeners();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.localScale = _originalScale * _hoverScale;
+        PlayHoverSound();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale = _originalScale;
     }
 
     private void SetDefaultUI()
@@ -35,21 +52,19 @@ public class UIButton : MonoBehaviour
 
     private void InitUIButton()
     {
-        if( _buttonBase != null )
+        if (_buttonBase != null)
         {
             return;
         }
-
         _buttonBase = GetComponentInChildren<Button>();
     }
 
     public void BindOnClickButtonEvent(Action onClickCallback)
     {
-        if(_buttonBase == null)
+        if (_buttonBase == null)
         {
             return;
         }
-
         _buttonBase.onClick.AddListener(new UnityEngine.Events.UnityAction(onClickCallback));
     }
 
@@ -59,17 +74,26 @@ public class UIButton : MonoBehaviour
         {
             return;
         }
-
         _buttonBase.onClick.RemoveListener(new UnityEngine.Events.UnityAction(onClickCallback));
     }
 
     public void OnClickSetSelectUI()
     {
-        if( _imageSelect != null)
+        if (_imageSelect != null)
         {
             bool currentActive = _imageSelect.gameObject.activeSelf;
             _imageSelect.gameObject.SetActive(!currentActive);
         }
+    }
+
+    private void PlayHoverSound()
+    {
+        SoundManager.Instance.PlaySFX("Assets/Sound/Button_1");
+    }
+
+    private void PlayClickSound()
+    {
+        SoundManager.Instance.PlaySFX("Assets/Sound/Button_2");
     }
 
     public void ChangeButtonSprite(Sprite sprite)
