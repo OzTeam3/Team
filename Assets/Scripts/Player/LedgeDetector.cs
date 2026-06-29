6131e15103cf1eeb8c021f10bd365003f301626f
정명진 <audwls3159@gmail.com>
2026-06-29 16:05:56 +0900

    feat: [플레이어] 그랩 감지 기능 구현

---
+++ Assets/Scripts/Player/LedgeDetector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class LedgeDetector : MonoBehaviour
+{
+    public event Action<Collider> LedgeTriggeredEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ledge")) // 태그 그라운드로 
+        {
+            Debug.Log("충돌중");
+            LedgeTriggeredEvent?.Invoke(other);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Ledge"))
+        {
+            LedgeTriggeredEvent?.Invoke(null); // 괄호안에 널투입
+        }
+    }
+}
+
+//그라운드 확인 -콜라이더 가져오기 -  모서리 알아서 -  
