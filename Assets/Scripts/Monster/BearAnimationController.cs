4c8099398c5678e26b16216ff008d9302c50080e
원도일 <wondoil1234@gmail.com>
2026-06-30 15:24:35 +0900

    refactor: [몬스터] 파일명 변경

---
+++ Assets/Scripts/Monster/BearAnimationController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class BearAnimationController
+{
+    private readonly Animator _animator;
+
+    public BearAnimationController(Animator animator)
+    {
+        _animator = animator;
+
+    }
+
+    public void PlayMove(bool isWalking)
+    {
+        if (_animator != null)
+        {
+            _animator.SetBool("isWalking", isWalking);
+        }
+    }
+
+    public void PlayChase(bool isChasing)
+    {
+        if (_animator != null)
+        {
+            _animator.SetBool("isChasing", isChasing);
+        }
+    }
+
+    public void PlayAttack(bool isAttacking)
+    {
+        if (_animator != null)
+        {
+            _animator.SetBool("isAttacking", isAttacking);
+        }
+    }
+}
