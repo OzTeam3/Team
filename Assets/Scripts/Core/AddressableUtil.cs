db37eb7fc3a380db5807a778784b22c5ae5c74bb
이주헌 <Volume6214@gmail.com>
2026-07-02 08:58:59 +0900

    feat: [Data] Sound, GameSetting, UI 데이터 추가, 관련 스크립트 수정
    - 외 데이터 수정

--- Assets/Scripts/Core/AddressableUtil.cs
+++ Assets/Scripts/Core/AddressableUtil.cs
@@ -1,19 +1,63 @@
-﻿
-//어드레서블 이주헌님이 다해주세요
-public class AddressableUtil
+﻿public class AddressableUtil
 {
-    public static class AddressPath
+    public static class DataPath
     {
         public const string Character = "JsonOutput/Character";
         public const string Item = "JsonOutput/Item";
+        public const string ItemStatUp = "JsonOutput/ItemStatUp";
         public const string Trap = "JsonOutput/Trap";
         public const string Monster = "JsonOutput/Monster";
-        public const string Map = "JsonOutput/Map";
         public const string Zone = "JsonOutput/Zone";
+        public const string UI = "JsonOutput/UI";
+        public const string GameSetting = "JsonOutput/GameSetting";
+    }
 
-        public static string GetZonePath(string zoneName)
-        {
-            return $"JsonOutput/{zoneName}";
-        }
+    public static class ZonePath
+    {
+        public const string Ground = "JsonOutput/Ground";
+        public const string FirstStage = "JsonOutput/FirstStage";
+        public const string SecondStage = "JsonOutput/SecondStage";
+        public const string ThirdStage = "JsonOutput/ThirdStage";
+        public const string WayUp = "JsonOutput/WayUp";
+        public const string Map = "JsonOutput/Map";
+    }
+
+    public static class UIPath
+    {
+        public const string ESCPopupUI = "JsonOutput/ESCPopupUI";
+        public const string FadePopupUI = "JsonOutput/FadePopupUI";
+        public const string MainHUD = "JsonOutput/MainHUD";
+        public const string TitleUI = "JsonOutput/TitleUI";
+        public const string SettingPopupUI = "JsonOutput/SettingPopupUI";
+        public const string StartPopupUI = "JsonOutput/StartPopupUI";
+        public const string EndingPopupUI = "JsonOutput/EndingPopupUI";
+    }
+
+    public static class SoundPath
+    {
+        public const string Bgm1 = "JsonOutput/Bgm1";
+        public const string Bgm2 = "JsonOutput/Bgm2";
+        public const string Title = "JsonOutput/Title";
+        public const string Button1 = "JsonOutput/Button1";
+        public const string Button2 = "JsonOutput/Button2";
+    }
+
+    public static class GameSettingPath
+    {
+        public const string StartPosition = "JsonOutput/StartPosition";
+        public const string FirstSave = "JsonOutput/FirstSave";
+        public const string SecondSave = "JsonOutput/SecondSave";
+        public const string ThirdSave = "JsonOutput/ThirdSave";
+        public const string FinalPosition = "JsonOutput/FinalPosition";
+
+    }
+    public static string GetZonePath(string zoneName)
+    {
+        return $"JsonOutput/{zoneName}";
+    }
+
+    public static string GetUIPath(string uiName)
+    {
+        return $"JsonOutput/{uiName}";
     }
 }
--- Assets/Scripts/Core/GameData.cs
+++ Assets/Scripts/Core/GameData.cs
@@ -1,4 +1,38 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ZoneType
+{
+    None,
+    Stage,
+    UnStage
+}
+
+//public enum UIRootType
+//{
+//    BackgroundUI,
+//    MainUI,
+//    ContentUI,
+//    PopupUI,
+//    VeryFrontUI
+//}
+
+//public enum UIType
+//{
+//    ESCPopupUI,
+//    FadePopupUI,
+//    MainHUD,
+//    TitleUI,
+//    SettingPopupUI,
+//    StartPopupUI
+//}
+
+[Serializable]
+public class SerializationWrapper<T>
+{
+    public List<T> items;
+}
 
 [System.Serializable]
 public class GameDataBase
@@ -10,8 +44,8 @@ public class GameDataBase
 public class CharacterData : GameDataBase
 {
     public string Name;
-    public int MoveSpeed;
-    public int JumpForce;
+    public float MoveSpeed;
+    public float JumpForce;
     public string PrefabPath;
 }
 
@@ -28,10 +62,10 @@ public class ItemData : GameDataBase
 public class TrapData : GameDataBase
 {
     public string Name;
-    public int ActionValue;
-    public int KnockbackForce;
-    public int Value1;
-    public int Value2;
+    public float ActionValue;
+    public float KnockbackForce;
+    public float Value1;
+    public float Value2;
     public string PrefabPath;
 }
 
@@ -39,12 +73,12 @@ public class TrapData : GameDataBase
 public class MonsterData : GameDataBase
 {
     public string Name;
-    public int MoveSpeed;
-    public int ChaseSpeed;
-    public int PatrolRadius;
-    public int DetectRadius;
-    public int AttackRadius;
-    public int PushForce;
+    public float MoveSpeed;
+    public float ChaseSpeed;
+    public float PatrolRadius;
+    public float DetectRadius;
+    public float AttackRadius;
+    public float PushForce;
     public string PrefabPath;
 }
 
@@ -52,10 +86,23 @@ public class MonsterData : GameDataBase
 public class ZoneData : GameDataBase
 {
     public string Name;
-    public int MinY;
-    public int MaxY;
+    public ZoneType Type;
+    public float MinY;
+    public float MaxY;
+    public float PositionY;
+    public float OffsetX;
+    public float OffsetY;
+    public float OffsetZ;
     public string PrefabPath;
 
     public bool IsLoaded { get; set; } = false;
 }
 
+[System.Serializable]
+public class UIData : GameDataBase
+{
+    public string Name;
+    public UIRootType UIRootType;
+    public string UIType;
+    public string PrefabPath;
+}
